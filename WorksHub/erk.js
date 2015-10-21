fc2c504b2cff6179392ef1200bc63b7cde865400
Joseph Mwangi <mwangiplus@gmail.com>
2015-10-21 15:04:09 +0300

    I Do

---
+++ WorksHub/erk.js
@@ -0,0 +1,31 @@
+<html>
+<head>
+<script>
+x();
+function x(){
+
+for(var i=0;i<arguments[i].length;i++ ){
+var k = 0;
+if(arguments[i]/2==0){
+k++;
+}
+
+}
+alert(k);
+
+
+
+}	
+
+
+
+
+</script>
+</head>
+<body>
+
+
+</body>
+
+
+</html>
